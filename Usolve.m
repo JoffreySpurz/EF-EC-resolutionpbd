b9185496395d33c4e49e655234dd3cbe30670723
Joffrey <j.chaneac@sfr.fr>
2019-01-16 08:24:43 +0100

    Stockage de la résolution à chaque étape de la chaleur dans U

--- Usolve.m
+++ Usolve.m
@@ -1,28 +1,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% A : Matrice (nP x nP) du membre de gauche
-% Mc : Matrice (nP x nP) de masse modifiee
+% B : Matrice (nP x nP) de la forme A\Mc
 % Fc : Vecteur (nP x 1) du laser
-% I : liste des points du bord
 % U : matrice de la temperature dans la plaque
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [ U ] = Usolve(A, Mc, Fc, I, U)
-%%% Prise en compte des CL %%%
-% Nombre de points du bord
-nI = length(I);
-
-% CL de Dirichlet
-A(I,:) = 0;
-A(I,I) = speye(nI);
-Mc(I,:) = 0;
-Mc(I,I) = speye(nI);
-%Fc(I) = 0;% utile ?
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ U ] = Usolve(B, Fc, niter)
+%%% Condition initiale %%%
+U = sparse(nP, niter); 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %%% Resolution %%%
-%U = A\(Mc*(U+Fc));% Donnee par les tuteurs
-U = A\(Mc*U+Fc);% Bonne formulation ?
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i=2:niter
+    U(:,k) = B*(U(:,k-1)+Fc);
+end
+%%%%%%%%%%%%%%%%%%%%%
 end
 
