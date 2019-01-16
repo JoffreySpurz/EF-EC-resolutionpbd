9a095246493e5d51b16586ab87895ea2136004cd
Joffrey <j.chaneac@sfr.fr>
2019-01-16 08:45:47 +0100

    Résolution du problème direct en sauvegardant U et mesh1

--- Plot_Sans_Fissure.m
+++ Plot_Sans_Fissure.m
@@ -7,86 +7,22 @@ clear; close all;
 %%% maillage fixe.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%% Maillage %%%%%%%%%%
-% 1 Unit square
-domain1 = Domain('square');
-
-% mesh
-dx = 0.1; % taille d'un element
-mesh1 = Mesh(domain1,dx);
-
-% Liste des sommets
-P = mesh1.nodes;
-
-% Liste des triangles
-T = mesh1.triangles;
-
-% Liste des points du bords
-I = mesh1.boundary;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%% Load %%%%%%%%%%
+% Resolution
+load('Probleme_direct_sans_fissure','U', 'mesh1')
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%% Parametres %%%%%%%%%%
-% Nombre de sommets
-nP = length(P); 
-
-% Nombre de triangles
-nT = length(T);
-
-% Condition initiale
-U = sparse(nP, 1); 
-
-% Plaque
-rho_plaque = 7850;% masse volumique (Kg/m^3) : valeur de l'acier
-cp_plaque = 444;% capacite thermique massique (J/K/Kg) : valeur de l'acier
-beta_plaque = rho_plaque*cp_plaque; % rho*cp dans la plaque
-lambda_plaque = 50.2; % conductivite thermique (W/m/K) : valeur de l'acier 
-
-% Iteration
-niter = 15; % Nombre d'iterations pour la resolution
-dt = 0.1; % Pas en temps
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%% Matrices d'iterations %%%%%%%%
-%%% Raideur
-% Paramètre : Rho*cp sur chaque triangle
-LAMBDA = mesh1.P0(lambda_plaque,2);
-% Matrice de raideur
-Kc = dt*mesh1.stiffness(LAMBDA);%matriceK(P, T, Lambda, dt);
-
-
-%%% Masse
-% Paramètre : Conductivite thermique (lambda) sur les triangles
-BETA = mesh1.P0(beta_plaque);
-% Matrice de masse
-M = mesh1.mass(BETA);
-%mesh1.mass(Beta.*ones(nT,1));
-
-
-%%% Membre de gauche
-% Probleme AX(n) = MX(n-1) + F
-A = M + Kc;
-
-
-%%% Laser
-Sv = 8*10^9 ; % Puissance volumique du laser applique a la plaque (W/m^3)
-S = Sv *10^(-5); % Puissance volumique du laser utilisee
-% Second membre
-Fc = dt*S*mesh1.P1('x.^2+y.^2<0.2^2');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%%%%%%%%% Resolution %%%%%%%%%%%
+%%%%%%%%% Affichage %%%%%%%%%%%
+niter = length(U(1,:));
+pas = 1;
 
 %%% Avec laser
-for i = 1:niter
+for i = 1:pas:niter
     %%% Affichage
-    mesh1.surf(U)
-    caxis([0 1]*10^(-2))
-    pause(0.1)
-    
-    %%% Iteration
-    U = Usolve(A, M, Fc, I, U);
+    mesh1.surf(U(:,i))
+    %caxis([0 0.05])
+    pause(0.01)
 end
 
 % % On indique que le laser va etre coupe
@@ -104,15 +40,3 @@ end
 %     U = Usolve(A, M, zeros(nP,1), I, U);
 % end
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%%%%%%%%%%% Affichage au temps final %%%%%%%%%
-% mesh1.surf(U)
-% caxis([0 1000])
-% 
-% figure;
-% mesh1.plot
-
-% figure;
-% mesh1.plot
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ Probleme_Direct_Sans_Fissure.m
@@ -0,0 +1,96 @@
+clear; close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%     Resolution de l'equation de la chaleur avec 
+%%% conditions aux limites de Dirichlet homogene, 
+%%% par la methode des elements finis P1 et un
+%%% maillage fixe.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%% Maillage %%%%%%%%%%
+% 1 Unit square
+domain1 = Domain('square');
+
+% mesh
+dx = 0.1; % taille d'un element
+mesh1 = Mesh(domain1,dx,'save');
+
+% Liste des sommets
+P = mesh1.nodes;
+
+% Liste des triangles
+T = mesh1.triangles;
+
+% Liste des points du bords
+I = mesh1.boundary;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%% Parametres %%%%%%%%%%
+% Nombre de sommets
+nP = length(P); 
+
+% Nombre de triangles
+nT = length(T);
+
+% Plaque
+rho_plaque = 7850;% masse volumique (Kg/m^3) : valeur de l'acier
+cp_plaque = 444;% capacite thermique massique (J/K/Kg) : valeur de l'acier
+beta_plaque = rho_plaque*cp_plaque; % rho*cp dans la plaque
+lambda_plaque = 50.2; % conductivite thermique (W/m/K) : valeur de l'acier 
+
+% Iteration
+niter = 15; % Nombre d'iterations pour la resolution
+dt =10^(-6); % Pas en temps
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%% Matrices d'iterations %%%%%%%%
+%%% Raideur
+% Paramètre : Rho*cp sur chaque triangle
+LAMBDA = mesh1.P0(1,2);
+% Matrice de raideur
+Kc = dt*mesh1.stiffness(LAMBDA);%matriceK(P, T, Lambda, dt);
+
+
+%%% Masse
+% Paramètre : Conductivite thermique (lambda) sur les triangles
+BETA = mesh1.P0(1);
+% Matrice de masse
+M = mesh1.mass(BETA);
+%mesh1.mass(Beta.*ones(nT,1));
+
+
+%%% Membre de gauche
+% Probleme AX(n) = M( X(n-1) + F )
+A = M + Kc;
+
+%%% Prise en compte des CL %%%
+% Nombre de points du bord
+nI = length(I);
+
+% CL de Dirichlet
+A(I,:) = 0;
+A(I,I) = speye(nI);
+Mc(I,:) = 0;
+Mc(I,I) = speye(nI);
+
+% Probleme X(n) = A\M ( X(n-1) + F ) = B ( X(n-1) + F )
+B = A\M;
+
+
+%%% Laser
+Sv = 8*10^9 ; % Puissance volumique du laser applique a la plaque (W/m^3)
+S = Sv *10^(-5); % Puissance volumique du laser utilisee
+% Second membre
+Fc = dt*S*mesh1.P1('exp(-(x.^2+y.^2)/0.1)');% Gaussien
+%Fc=dt*S*mesh1.P1('x.^2+y.^2<0.2^2');% Cercle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%% Resolution %%%%%%%%%%%
+%%% Avec laser
+U = Usolve(B, Fc, nP, niter);
+
+%%% Save
+save('Probleme_direct_sans_fissure','U', 'mesh1')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
