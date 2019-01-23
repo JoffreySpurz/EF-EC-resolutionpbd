12eed8ab83f4bc837774142267ac08055a9628cb
Joffrey <j.chaneac@sfr.fr>
2019-01-23 10:25:46 +0100

    Suppression de la boucle for au profit d'un travail matriciel

--- Probleme_Inverse/adjoint_beta.m
+++ Probleme_Inverse/adjoint_beta.m
@@ -1,24 +1,24 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % U : matrice de la temperature dans la plaque
-% U : matrice de psi(x,t)
+% Psi : matrice de psi(x,t)
 % B : Matrice (nP x nP) de la forme A\Mc
 % nP : Nombre de sommets du maillage
 % niter : Nombre d'iterations pour la resolution
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [ grad ] = adjoint_beta(U,Psi,B,nP,niter)
+function [ Adjoint ] = adjoint_beta(U,Psi,B,nP,niter)
 %%% Obtention de Phi(x,T-t)
 % Resolution de l'equation de la chaleur
 Phi = Usolve(B, Psi, nP, niter);
 % Changement de variables t->T-t
 Phi = fliplr(Phi);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% Calcul du gradient : grad = Dt(u) Phi(x,T-t)
-grad = zeros(nP,niter);
-for k = 2:niter
-    grad(:,k)= (U(:,k)-U(:,k-1))/dt .* Phi(:,k);
-end
-%%%%%%%%%%%%%%%%%%%%%%%
+%%% Calcul de l'adjoint de la différentielle : Adjoint = Dt(u) Phi(x,T-t)
+% Troncature sur U(t) pour obtenir U(t-dt)
+Utronc = [zeros(nP,1) U(:,1:niter-1)];
+% adjoint de la differentielle
+Adjoint = (U-Utronc)/dt .* Phi;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 
