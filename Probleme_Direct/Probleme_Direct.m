c6d87d1efde42c860cfcc88395ed27a389252223
Joffrey <j.chaneac@sfr.fr>
2019-02-09 11:51:20 +0100

    Adimensionnement forme gaussienne du laser => correction de la dépendance du maillage

--- Probleme_Direct/Probleme_Direct.m
+++ Probleme_Direct/Probleme_Direct.m
@@ -130,7 +130,8 @@ Sv = 8*10^9 ; % Puissance volumique du laser applique a la plaque (W/m^3)
 S = Sv*l^3/(lambda_plaque*l); % Puissance volumique du laser utilisee
 % Second membre 
 sigma = 0.005;
-Fc = M*dt*S/sqrt(2*pi*sigma^2)*mesh1.P1(['exp(-0.5*(x.^2+y.^2)/' num2str(sigma^2) ')']);% Gaussien
+F = mesh1.P1(['exp(-0.5*(x.^2+y.^2)/' num2str(sigma^2) ')']);% Gaussien
+Fc = M*dt*S/sqrt(2*pi*sigma^2)*(F/max(F)) ;
 %Fc=dt*S*mesh1.P1('x.^2+y.^2<0.2^2');% Cercle
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
